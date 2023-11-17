ca5522dc5a183f4cee56c52734b763a5c1b4d894
MandingoBrandon <brandonbalchand@gmail.com>
2023-11-16 23:16:28 -0500

    Blockchain code progress

--- backend/blockchain/RealEstateToken.sol
+++ backend/blockchain/RealEstateToken.sol
@@ -1,12 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-/*
-This contract allows the owner to set the initial total supply, share price, 
-and the total number of shares (10,000 shares in this case). The onlyOwner modifier 
-allows only the owner can change the share price. Users can buy shares by sending Ether,
- and the ownership is transferred from the owner to the buyer. You can deploy this 
- contract, and the owner can set the initial parameters and interact with it accordingly.
- */
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -14,14 +7,35 @@ contract RealEstateToken is ERC20 {
     address public owner;
     uint256 public totalShares;
     uint256 public sharePrice;
+    uint256 public totalProperties;
+    uint256 public totalAuctions;
 
-    // Mapping to track the number of shares owned by each user
-    mapping(address => uint256) public shares;
+    struct Property {
+        address owner;
+        uint256 sharesAvailable;
+        uint256 listPrice;
+        uint256 estimatedPrice;
+        mapping(address => uint256) investorShares;
+        bool isPublic;
+        bool isAuctioned;
+    }
+
+    struct Auction {
+        uint256 propertyId;
+        address highestBidder;
+        uint256 highestBid;
+        bool ended;
+    }
+
+    mapping(uint256 => Property) public properties;
+    mapping(uint256 => Auction) public auctions;
 
     constructor(string memory _name, string memory _symbol, uint256 _initialSupply, uint256 _sharePrice, uint256 _totalShares) ERC20(_name, _symbol) {
         owner = msg.sender;
         totalShares = _totalShares;
         sharePrice = _sharePrice;
+        totalProperties = 0;
+        totalAuctions = 0;
         _mint(owner, _initialSupply);
     }
 
@@ -34,15 +48,91 @@ contract RealEstateToken is ERC20 {
         sharePrice = _newPrice;
     }
 
-    function buyShares(uint256 numShares) external payable {
-        require(numShares > 0, "Number of shares must be greater than zero");
-        require(msg.value >= numShares * sharePrice, "Insufficient funds");
-        require(totalShares >= numShares, "Not enough shares available");
+function addProperty(uint256 _listPrice, uint256 _estimatedPrice, uint256 _initialShares, bool _isPublic) public onlyOwner {
+    totalProperties++;
 
-        uint256 cost = numShares * sharePrice;
-        shares[msg.sender] += numShares;
-        totalShares -= numShares;
-        _transfer(owner, msg.sender, numShares);
-        payable(owner).transfer(cost);
+    Property storage newProperty = properties[totalProperties];
+    newProperty.owner = msg.sender;
+    newProperty.sharesAvailable = _initialShares;
+    newProperty.listPrice = _listPrice;
+    newProperty.estimatedPrice = _estimatedPrice;
+    newProperty.isPublic = _isPublic;
+    newProperty.isAuctioned = false;
+}
+
+    function startAuction(uint256 _propertyId) public {
+        require(properties[_propertyId].owner == msg.sender, "Only the property owner can start an auction");
+        require(!properties[_propertyId].isAuctioned, "Auction for this property has already started");
+
+        totalAuctions++;
+        auctions[totalAuctions] = Auction({
+            propertyId: _propertyId,
+            highestBidder: address(0),
+            highestBid: 0,
+            ended: false
+        });
+
+        properties[_propertyId].isAuctioned = true;
     }
+
+    function placeBid(uint256 _auctionId) external payable {
+        require(!auctions[_auctionId].ended, "Auction already ended");
+        require(msg.value > auctions[_auctionId].highestBid, "Bid must be higher than the current highest bid");
+
+        if (auctions[_auctionId].highestBidder != address(0)) {
+            payable(auctions[_auctionId].highestBidder).transfer(auctions[_auctionId].highestBid);
+        }
+
+        auctions[_auctionId].highestBidder = msg.sender;
+        auctions[_auctionId].highestBid = msg.value;
+    }
+
+    function endAuction(uint256 _auctionId) public {
+        require(properties[auctions[_auctionId].propertyId].owner == msg.sender, "Only the property owner can end the auction");
+        require(!auctions[_auctionId].ended, "Auction already ended");
+
+        auctions[_auctionId].ended = true;
+        properties[auctions[_auctionId].propertyId].owner = auctions[_auctionId].highestBidder;
+        properties[auctions[_auctionId].propertyId].isAuctioned = false;
+
+        _transfer(owner, auctions[_auctionId].highestBidder, properties[auctions[_auctionId].propertyId].investorShares[auctions[_auctionId].highestBidder]);
+    }
+
+    function buyShares(uint256 _propertyId, uint256 _numShares) external payable {
+        require(_numShares > 0, "Number of shares must be greater than zero");
+        require(msg.value >= _numShares * sharePrice, "Insufficient funds");
+        require(properties[_propertyId].sharesAvailable >= _numShares, "Not enough shares available");
+
+        uint256 cost = _numShares * sharePrice;
+        properties[_propertyId].investorShares[msg.sender] += _numShares;
+        properties[_propertyId].sharesAvailable -= _numShares;
+        _transfer(owner, msg.sender, _numShares);
+        payable(properties[_propertyId].owner).transfer(cost);
+    }
+
+function getPropertyOwner(uint256 _propertyId) public view returns (address) {
+    return properties[_propertyId].owner;
+}
+
+function getPropertySharesAvailable(uint256 _propertyId) public view returns (uint256) {
+    return properties[_propertyId].sharesAvailable;
+}
+
+function getPropertyListPrice(uint256 _propertyId) public view returns (uint256) {
+    return properties[_propertyId].listPrice;
+}
+
+function getPropertyEstimatedPrice(uint256 _propertyId) public view returns (uint256) {
+    return properties[_propertyId].estimatedPrice;
+}
+
+function isPropertyPublic(uint256 _propertyId) public view returns (bool) {
+    return properties[_propertyId].isPublic;
+}
+
+function isPropertyAuctioned(uint256 _propertyId) public view returns (bool) {
+    return properties[_propertyId].isAuctioned;
+}
+
+    // Implement AI estimate functions, stake management, and other functionalities as required
 }
