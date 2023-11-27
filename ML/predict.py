99d5d27ac6445a3a08da3208a5c3fba2f3346990
Charlie <charlieliu1016@gmail.com>
2023-11-27 14:42:53 -0500

    include location for prediction model

--- ML/predict.py
+++ ML/predict.py
@@ -2,28 +2,31 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_selector
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
 
 df = pd.read_csv('Troy_Housing.csv')
 
-features = ['description_beds', 'description_baths_consolidated', 'description_lot_sqft', 'description_sqft', 'list_price']
+numerical_features = ['description_sqft', 'description_baths_consolidated', 'description_lot_sqft', 'description_beds', 'list_price']
+categorical_features = ['location_address_state', 'location_county_name', 'location_address_city']
 target = 'description_sold_price'
 
-X = df[features]
-y = df[target]
+X_train, X_test, y_train, y_test = train_test_split(df[numerical_features + categorical_features], df[target], test_size=0.2, random_state=42, shuffle=False)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+numeric_transformer = StandardScaler()
+categorical_transformer = OneHotEncoder(drop='first', handle_unknown='ignore')
 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+preprocessor = make_column_transformer(
+    (numeric_transformer, make_column_selector(dtype_include='number')),
+    (categorical_transformer, make_column_selector(dtype_exclude='number'))
+)
 
-model = LinearRegression()
+model = make_pipeline(preprocessor, LinearRegression())
+model.fit(X_train, y_train)
 
-model.fit(X_train_scaled, y_train)
-
-y_pred = model.predict(X_test_scaled)
-
+y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 print(f'Mean Squared Error: {mse}')
 
@@ -32,10 +35,11 @@ new_data = pd.DataFrame({
     'description_baths_consolidated': [2],
     'description_lot_sqft': [1307],
     'description_sqft': [49000],
-    'list_price': [600000]
+    'list_price': [100000],
+    'location_address_state': ['New York'],
+    'location_county_name': ['Rensselaer'],
+    'location_address_city': ['Troy']
 })
 
-new_data_scaled = scaler.transform(new_data)
-
-predicted_price = model.predict(new_data_scaled)
+predicted_price = model.predict(new_data)
 print(f'Predicted Price: {predicted_price[0]}')
